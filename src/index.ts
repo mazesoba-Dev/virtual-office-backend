08a56bd550c9a2ea70e32cc6d170be684e99c1b8
thehighhigh <Yuto.ld.1017@gmail.com>
2022-11-26 11:45:24 +0900

    use cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import {ReplicacheExpressServer} from 'replicache-express';
 import type { WriteTransaction } from 'replicache';
+import cors from 'cors'
+
 
 const port = 3000
 const mutators = {
@@ -11,12 +13,38 @@ const mutators = {
     });
   }
 }
+
+
 const options = {
   mutators,
   port,
   host: process.env.HOST || '0.0.0.0',
 };
 
-ReplicacheExpressServer.start(options, () => {
-  console.log(`Server listening on ${options.host}:${options.port}`);
+// const r = ReplicacheExpressServer.start(options, () => {
+//   console.log(`Server listening on ${options.host}:${options.port}`);
+// });
+
+
+const r = new ReplicacheExpressServer(options);
+
+// r.app.use(express.static(default_dist));
+r.app.get('/health', (_req, res) => {
+  res.send('ok');
+});
+
+r.app.get('/', (_req, res) => {
+  res.send('ok');
+});
+
+r.app.use(cors({
+  origin: 'http://localhost:3001', //アクセス許可するオリジン
+  credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+  optionsSuccessStatus: 200 //レスポンスstatusを200に設定
+}))
+
+r.start(() => {
+  console.log(
+    `Replicache is listening on ${options.host}:${options.port}`,
+  );
 });
